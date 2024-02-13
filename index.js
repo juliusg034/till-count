06bbff53ecbe8831ae2a180979139e982c58bc4e
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-02-12 23:43:18 -0800

    changed the implementation of the depositAmount function to include the use of an object to make passing the concurrent deposit value between calls easier to see

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function depositAmount(deposit, denomination, amount){
   if(amount === 0){return [deposit, 0]}
-  else if (deposit === 0) {return [deposit, 0]}
+  else if (deposit === 0) {return  0}
   else if (amount > 0){
-    let maxAmount = Math.floor(deposit/ denomination);
+    let maxAmount = Math.floor(deposit/denomination);
     if(maxAmount > amount){
       deposit = deposit - (amount * denomination);
-      return [deposit, amount];
+      return  amount;
     } else if (maxAmount < amount){
       deposit = deposit - (maxAmount * denomination);
-      return [deposit, maxAmount];
+      return  maxAmount;
     }
   }
 
@@ -24,7 +24,7 @@ function getValues() {
   const coins = parseFloat(document.getElementById('coins').value);
 
   let totalsum = hundreds + fifties + twenties + tens + fives + ones + coins; 
-  let deposit = totalsum - 300;  
+  let deposit = {amount: totalsum};
   let amountFives = fives / 5
   let amountTens = tens / 10
   let amountTwenties = twenties / 20
@@ -32,35 +32,12 @@ function getValues() {
   let amountHundreds = hundreds / 100
   let amountOnes = ones / 1
 
-  
-  returnVal = depositAmount(deposit, 100, amountHundreds);
-  deposit = returnVal[0];
-  let depositHundreds = returnVal[1];
-
-
-  returnVal = depositAmount(deposit, 50, amountFifties);
-  deposit = returnVal[0];
-  let depositFifties = returnVal[1];
-
-
-  returnVal = depositAmount(deposit, 20, amountTwenties);
-  deposit = returnVal[0];
-  let depositTwenties = returnVal[1];
-
-
-  returnVal = depositAmount(deposit, 10, amountTens);
-  deposit = returnVal[0];
-  let depositTens = returnVal[1];
-
-  
-  returnVal = depositAmount(deposit, 5, amountFives);
-  deposit = returnVal[0];
-  let depositFives = returnVal[1];
- 
-
-  returnVal = depositAmount(deposit, 1, amountOnes);
-  deposit = returnVal[0];
-  let depositOnes = returnVal[1];
+  depositHundreds = depositAmount(deposit.amount, 100, amountHundreds);
+  depositFifties = depositAmount(deposit.amount, 50, amountFifties);
+  depositTwenties = depositAmount(deposit.amount, 20, amountTwenties);
+  depositTens = depositAmount(deposit.amount, 10, amountTens);
+  depositFives = depositAmount(deposit.amount, 5, amountFives);
+  depositOnes = depositAmount(deposit.amount, 1, amountOnes);  
 
 
   const part1 = document.getElementById('deposit1');
